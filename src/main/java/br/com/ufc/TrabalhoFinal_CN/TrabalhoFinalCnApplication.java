e1b78e59aa022cf533993ff7d9b57c8fb53de9ac
keane032 <keane032@gmail.com>
2019-05-06 14:51:48 -0300

    add dev

---
+++ src/main/java/br/com/ufc/TrabalhoFinal_CN/TrabalhoFinalCnApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufc.TrabalhoFinal_CN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalCnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalCnApplication.class, args);
+	}
+
+}
