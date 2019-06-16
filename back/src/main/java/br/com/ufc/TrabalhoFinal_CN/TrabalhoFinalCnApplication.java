d87ade86eb0d03118369808e97412513ce43797b
keane032 <keane032@gmail.com>
2019-06-15 23:58:39 -0300

    ..

--- back/src/main/java/br/com/ufc/TrabalhoFinal_CN/TrabalhoFinalCnApplication.java
+++ back/src/main/java/br/com/ufc/TrabalhoFinal_CN/TrabalhoFinalCnApplication.java
@@ -2,17 +2,23 @@ package br.com.ufc.TrabalhoFinal_CN;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TrabalhoFinalCnApplication {
+public class TrabalhoFinalCnApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhoFinalCnApplication.class, args);
 	}
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrabalhoFinalCnApplication.class);
+	}   
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
